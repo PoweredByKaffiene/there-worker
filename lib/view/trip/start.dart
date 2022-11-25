9e11e33272b09e0652c81d9500d3043c4d69cd5d
Syed Ahkam <smahkam57@gmail.com>
2022-11-25 15:05:07 +0530

    Hook up poduct id scanning

--- lib/view/trip/start.dart
+++ lib/view/trip/start.dart
@@ -1,16 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 import '../../widgets/layout/root.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  StartScreen({super.key});
+
+  MobileScannerController cameraController = MobileScannerController();
+
+  void validateCode(String code) {
+    if (!code.contains(':')) return;
+
+    var parts = code.split(':');
+    if (parts.length != 2) return;
+
+    String label = parts[0];
+    String productId = parts[1];
+
+    if (!(label == 'THERE_PRODUCT_ID')) return;
+
+    print(productId);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Root(
-      child: Center(child: Text("Trip Start")),
+      appbar: AppBar(
+        title: const Text(
+          'Scan Product',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flash_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flash_on, color: Colors.yellow);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.toggleTorch(),
+          ),
+        ],
+      ),
+      child: MobileScanner(
+        controller: cameraController,
+        onDetect: (barcode, args) {
+          if (barcode.rawValue == null) {
+            debugPrint('failed to scan barcode');
+            return;
+          }
+
+          final String code = barcode.rawValue!;
+          validateCode(code);
+        },
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import flutter_secure_storage_macos
+import mobile_scanner
 import path_provider_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageMacosPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
